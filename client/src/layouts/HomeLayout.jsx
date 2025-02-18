ff2c4663713d7ce221b76b1bf261aa20568e7133
M-Azariel <walter.azariel.moreno@gmail.com>
2025-02-18 01:16:06 -0300

    banner con navbar responsive

--- client/src/layouts/HomeLayout.jsx
+++ client/src/layouts/HomeLayout.jsx
@@ -1,16 +1,16 @@
+import React from "react";
 import { Outlet } from 'react-router-dom'
 import { Header } from '../components/Header'
 
 const HomeLayout = () => {
   return (
     <div>
-        <h1 >gika</h1>
-    <header>
-        <Header></Header>
-    </header>
-    <main>
-        <Outlet/>
-    </main>
+        <header>
+            <Header></Header>
+        </header>
+        <main>
+            <Outlet/>
+        </main>
     </div>
   )
 }
