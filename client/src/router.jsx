2fd470a4b51515157b1d614272619a334a641d29
Alejandro <aletomasperren@gmail.com>
2025-02-17 23:54:54 -0300

    Fix error React

--- client/src/router.jsx
+++ client/src/router.jsx
@@ -1,4 +1,4 @@
-
+import React from 'react'
 import { createBrowserRouter } from "react-router-dom";
 import HomeLayout from "./layouts/HomeLayout";
 import Home from "./pages/Home";
@@ -6,8 +6,9 @@ import ListOfProperties from "./pages/ListOfProperties";
 import PropertiePage from "./pages/PropertiePage";
 import AdminLayout from "./layouts/AdminLayout";
 import PropertiesForm from "./pages/PropertiesForm";
-//Home Templates
 
+
+//Home Templates
 export const router = createBrowserRouter([
     //Home 
     {
@@ -21,11 +22,11 @@ export const router = createBrowserRouter([
             element: <Home/>
         },
         {
-            path: "/inmuebles",
+            path: "inmuebles",
             element: <ListOfProperties/>
         },
         {
-            path: "/inmueble",
+            path: "inmueble",
             element: <PropertiePage/>
         }
     ],
@@ -41,7 +42,7 @@ export const router = createBrowserRouter([
 
             },
             {
-                path: "/crearInmueble",
+                path: "crearInmueble",
                 element: <PropertiesForm/>
             }
         ]
