27de25f057ef0c92300f519fb38a2ca355c1e63e
AlejandroPerren <aletomasperren@gmail.com>
2025-02-21 16:03:52 -0300

    Globales y NavBar Link

--- client/src/components/shared/NavBar.jsx
+++ client/src/components/shared/NavBar.jsx
@@ -1,32 +1,34 @@
-import React from 'react'
-import { Logo } from './Logo'
-import { useState } from 'react'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { Logo } from './Logo';
 
 export const NavBar = () => {
-    const [active, setActive] = useState(0)
-    const items = ['Inicio', 'Propiedades', 'Nosotros', 'Contacto'];
-
-
-    const handleClickLi=(index)=>{
-        setActive(index)
-    }
+    const items = [
+        { name: 'Inicio', path: '/' },
+        { name: 'Propiedades', path: '/inmuebles' },
+        { name: 'Nosotros', path: '/nosotros' },
+        { name: 'Contacto', path: '/contacto' }
+    ];
 
     return (
-        <nav className='w-full h-10 lg:h-20  flex justify-between items-center p-2 lg:px-20 bg-opacity-50 bg-[#333]'>
+        <nav className='w-full h-10 lg:h-20 flex justify-between items-center p-2 lg:px-20 bg-opacity-50 bg-[#333]'>
             <div>
-                <Logo/>
+                <Logo />
             </div>
             <ul className="flex gap-3 lg:gap-10 [&_li]:active:scale-95 [&_li]:hover:scale-105 [&_li]:text-xs [&_li]:lg:text-xl [&_li]:cursor-pointer transform transition duration-150">
-                {items.map((item,index)=>(
-                    <li
-                        key={index}
-                        onClick={()=>handleClickLi(index)}
-                        className={`p-1 lg:p-3  ${active === index ? "text-white" : "text-amber-300" }`}
-                    >
-                        {item}
+                {items.map((item, index) => (
+                    <li key={index}>
+                        <NavLink
+                            to={item.path}
+                            className={({ isActive }) =>
+                                `p-1 lg:p-3 ${isActive ? 'text-white' : 'text-amber-300'}`
+                            }
+                        >
+                            {item.name}
+                        </NavLink>
                     </li>
                 ))}
             </ul>
         </nav>
-    )
-}
+    );
+};
