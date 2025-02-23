b79916e7485fb803d4140f4abf6d9e1fe79570c0
M-Azariel <walter.azariel.moreno@gmail.com>
2025-02-23 20:08:37 -0300

    se mejoro la posicion del logo del footer en varias pantallas

--- client/src/components/Shared/Footer.jsx
+++ client/src/components/Shared/Footer.jsx
@@ -11,11 +11,11 @@ export const Footer = () => {
 
     return (
         <footer className='w-full h-10 lg:h-28 text-secundary bg-primary flex justify-between items-center px-2 lg:gap-10 xl:gap-0 xl:px-20'>
-            <div className="flex  items-center gap-10 lg:gap-[560px]">
-                <span className=' text-xs lg:text-lg xl:text-2xl  flex '>Copyright &copy; 2025 Urbania. </span>
+            <div className="flex  items-center gap-10 lg:gap-[160px] xl:gap-[500px]">
+                <span className=' text-xs lg:text-lg  xl:text-2xl  flex '>Copyright &copy; 2025 Urbania. </span>
                 <div>
                     <img src="/public/logo_Urbania-04.png" className="w-12 hidden xl:flex" alt="" />
-                    <img src="/public/logo_Urbania-03.png" className="w-42 flex xl:hidden  " alt="" />
+                    <img src="/public/logo_Urbania-03.png" className="w-20 lg:w-40 flex xl:hidden  " alt="" />
                 </div>
 
             </div>
