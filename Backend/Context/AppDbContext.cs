f7aafdd8ad3c973ee14e721b5417a4878a43272c
Jorge Trujillo <39315478+xiriuxb@users.noreply.github.com>
2025-03-04 14:11:12 -0500

    [fix] back: upadte AppDbContext

--- Backend/Context/AppDbContext.cs
+++ Backend/Context/AppDbContext.cs
@@ -11,7 +11,7 @@ namespace UrbaniaBackend.Context
         public DbSet<Inmobiliaria> Inmobiliaria { get; set; }
         public DbSet<Inmuebles> Inmueble { get; set; }
         public DbSet<User> Users { get; set; }
-        public DbSet<ContactFormType> ContactFormTypes { get; set; }
+        public DbSet<ContactFormType> ContactFormType { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
