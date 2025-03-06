3f537e3a9487b3646cbecae50b98839e87a6810a
Alejandro <aletomasperren@gmail.com>
2025-03-06 06:53:11 -0300

    Fetchs al back y creacion del .env

--- client/src/pages/admin/PropertiesForm.jsx
+++ client/src/pages/admin/PropertiesForm.jsx
@@ -4,6 +4,14 @@ import { useState } from "react";
 import { motion } from "framer-motion";
 import { PropertieSchema } from "../../schemas";
 import Error from "../../ui/ErrorMessage";
+import { createInquiry } from "../../network/fetchApiRealEstate";
+
+// {
+//   "name": "Inmuebles Big Out",
+//   "address": "avenida Siempre Viva",
+//   "phone": "3424243424",
+//   "email": "bigoutgmail.com"
+// }
 
 const PropertiesForm = () => {
   const {
@@ -15,10 +23,14 @@ const PropertiesForm = () => {
 
   const [imagenPrincipal, setImagenPrincipal] = useState(null);
 
-  const createProperty = (data) => {
-    console.log({ ...data, imagenPrincipal });
-    reset();
-    setImagenPrincipal(null);
+  const onSubmit = async (inquirity) => {
+    try {
+      const response = await createInquiry(inquirity);
+      console.log("propiedad creada con exito:", response);
+      reset();
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -30,8 +42,10 @@ const PropertiesForm = () => {
         transition={{ duration: 0.8, ease: "easeOut" }}
         className="max-w-lg min-h-[500px] mx-auto p-6 bg-white/65 shadow-lg rounded-lg backdrop-blur-sm relative md:min-h-[600px] sm:w-11/12"
       >
-        <h2 className="text-2xl font-bold text-center mb-4">Crea La Vivienda</h2>
-        <form onSubmit={handleSubmit(createProperty)}>
+        <h2 className="text-2xl font-bold text-center mb-4">
+          Crea La Vivienda
+        </h2>
+        <form onSubmit={handleSubmit(onSubmit)}>
           {[
             { name: "titulo", label: "Título", type: "text" },
             { name: "descripcion", label: "Descripción", type: "textarea" },
@@ -66,29 +80,31 @@ const PropertiesForm = () => {
           <div className="mb-6">
             <label className="block font-semibold">Imagen Principal</label>
             {!imagenPrincipal ? (
-  <input
-    type="file"
-    className="w-full p-2 border rounded"
-    onChange={(e) => setImagenPrincipal(e.target.files[0])}
-  />
-) : (
-  <div className="relative flex justify-center items-center">
-    <img
-      src={URL.createObjectURL(imagenPrincipal)}
-      alt="Principal"
-      className="w-full max-h-40 object-cover rounded shadow-md"
-    />
-    <button
-      type="button"
-      className="absolute top-2 right-2 bg-red-500 text-white px-2 rounded"
-      onClick={() => setImagenPrincipal(null)}
-    >
-      X
-    </button>
-  </div>
-)}
+              <input
+                type="file"
+                className="w-full p-2 border rounded"
+                onChange={(e) => setImagenPrincipal(e.target.files[0])}
+              />
+            ) : (
+              <div className="relative flex justify-center items-center">
+                <img
+                  src={URL.createObjectURL(imagenPrincipal)}
+                  alt="Principal"
+                  className="w-full max-h-40 object-cover rounded shadow-md"
+                />
+                <button
+                  type="button"
+                  className="absolute top-2 right-2 bg-red-500 text-white px-2 rounded"
+                  onClick={() => setImagenPrincipal(null)}
+                >
+                  X
+                </button>
+              </div>
+            )}
 
-            {errors.imagenPrincipal && <Error>{errors.imagenPrincipal.message}</Error>}
+            {errors.imagenPrincipal && (
+              <Error>{errors.imagenPrincipal.message}</Error>
+            )}
           </div>
 
           <motion.button
