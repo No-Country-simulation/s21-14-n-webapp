5b30cf8647b4f82f7749d23d367dfae4c07c704c
Alejandro <aletomasperren@gmail.com>
2025-03-08 15:52:55 -0300

    Fix image

--- client/src/pages/ListOfProperties.jsx
+++ client/src/pages/ListOfProperties.jsx
@@ -5,69 +5,6 @@ import ThemeProvider from "../context/ThemeProvider";
 import { getAllInquiries } from "../network/fetchApiInquirity";
 
 
-const propertyImages = [
-  {
-    imgSrc:
-      "https://images.pexels.com/photos/803975/pexels-photo-803975.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    imgAlt:
-      "Log house surrounded by threes, possibly in the middle of a forest, there is plants and bushed and leafs on the ground.",
-  },
-  {
-    imgSrc:
-      "https://images.pexels.com/photos/577697/pexels-photo-577697.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    imgAlt:
-      "Log house surrounded by threes, possibly in the middle of a forest, there is plants and bushed and leafs on the ground.",
-  },
-  {
-    imgSrc:
-      "https://images.pexels.com/photos/950058/pexels-photo-950058.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    imgAlt:
-      "Log house surrounded by threes, possibly in the middle of a forest, there is plants and bushed and leafs on the ground.",
-  },
-  {
-    imgSrc:
-      "https://images.pexels.com/photos/1144694/pexels-photo-1144694.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    imgAlt:
-      "Wodden cabin in the middle of a garden surrounded by trees and bushes.",
-  },
-  {
-    imgSrc:
-      "https://images.pexels.com/photos/2380247/pexels-photo-2380247.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    imgAlt:
-      "Inside view of a log house showing a window, branches outside, curtains, an old sofa chair and a lamp on top of a small furniture.",
-  },
-  {
-    imgSrc:
-      "https://images.pexels.com/photos/803975/pexels-photo-803975.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    imgAlt:
-      "Log house surrounded by threes, possibly in the middle of a forest, there is plants and bushed and leafs on the ground.",
-  },
-  {
-    imgSrc:
-      "https://images.pexels.com/photos/577697/pexels-photo-577697.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    imgAlt:
-      "Log house surrounded by threes, possibly in the middle of a forest, there is plants and bushed and leafs on the ground.",
-  },
-  {
-    imgSrc:
-      "https://images.pexels.com/photos/950058/pexels-photo-950058.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    imgAlt:
-      "Log house surrounded by threes, possibly in the middle of a forest, there is plants and bushed and leafs on the ground.",
-  },
-  {
-    imgSrc:
-      "https://images.pexels.com/photos/1144694/pexels-photo-1144694.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    imgAlt:
-      "Wodden cabin in the middle of a garden surrounded by trees and bushes.",
-  },
-  {
-    imgSrc:
-      "https://images.pexels.com/photos/2380247/pexels-photo-2380247.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    imgAlt:
-      "Inside view of a log house showing a window, branches outside, curtains, an old sofa chair and a lamp on top of a small furniture.",
-  },
-];
-
 const ListOfProperties = () => {
   const [allProperties, setAllProperties] = useState([]);
 
@@ -97,7 +34,7 @@ const ListOfProperties = () => {
           {allProperties.map((property, index) => (
             <PropertyCard
               key={index}
-              imgSrc={property.imageUrl}
+              imgSrc={property.imagePrincipal}
               title={property.title}
               price={property.price}
               address={property.address}
