87f12f58ef4039617c3ee6727a05ba708ce19e48
M-Azariel <walter.azariel.moreno@gmail.com>
2025-03-13 18:22:13 -0300

    cambios en la vista de propiedades y descripcion de propiedades

--- client/src/components/shared/PropertyCard.jsx
+++ client/src/components/shared/PropertyCard.jsx
@@ -9,9 +9,9 @@ const PropertyCard = ({ id, imgSrc, title, price, address }) => {
   };
 
   return (
-    <div className="w-full bg-white shadow-lg dark:bg-gray-800 dark:border-gray-700 cursor-pointer" onClick={handleClick}>
-      <figure className="relative h-40 group transition-all duration-500">
-        <img className="w-full h-full rounded-t-xs" src={imgSrc} alt={title} />
+    <div className="w-full bg-white shadow-xl shadow-black dark:bg-gray-800 dark:border-gray-700 cursor-pointer" onClick={handleClick}>
+      <figure className="relative h-96 w-auto group transition-all duration-500">
+        <img className="w-full h-full rounded-t-xs cont object-cover bg-center" src={imgSrc} alt={title} />
         <div className="absolute inset-0 bg-black/0 transition-all duration-500 pointer-events-none group-hover:bg-black/50 flex items-center justify-center">
           <span className="text-white font-light opacity-0 translate-y-2 group-hover:opacity-100 group-hover:translate-y-0 transition-all duration-700 ease-out">
             Ver Detalles
@@ -19,12 +19,12 @@ const PropertyCard = ({ id, imgSrc, title, price, address }) => {
         </div>
       </figure>
 
-      <div className="px-3 pb-3 pt-2 space-y-2 dark:bg-white dark:text-black">
-        <h2 className="block text-xs font-semibold tracking-tight text-gray-900 dark:text-black capitalize">
+      <div className="px-4 pb-3 pt-2 space-y-2 dark:backdrop-blur-sm dark:text-tertiary flex flex-col justify-center">
+        <h2 className="block text-lg font-semibold tracking-tight capitalize">
           {title}
         </h2>
         <section className="text-xs flex justify-between">
-          <span className="font-medium text-gray-900 dark:text-black">$ {price}</span>
+          <span className="text-lg">$ {price}</span>
           <span>{address}</span>
         </section>
       </div>
@@ -33,3 +33,5 @@ const PropertyCard = ({ id, imgSrc, title, price, address }) => {
 };
 
 export default PropertyCard;
+
+
--- client/src/pages/PropertyPage.jsx
+++ client/src/pages/PropertyPage.jsx
@@ -24,7 +24,7 @@ const PropertyPage = () => {
   if (!property) return <p className="text-center mt-10 text-lg">Cargando información...</p>;
 
   return (
-    <div className="bg-white dark:bg-primary dark:text-white transition-colors duration-500 ease-in-out">
+    <div className="bg-white dark:bg-primary flex flex-col dark:text-white transition-colors duration-500 ease-in-out">
       <ThemeSelect displayType="toggle" />
 
       {/* Título */}
@@ -41,23 +41,23 @@ const PropertyPage = () => {
           </div>
 
           {/* Detalles de la Propiedad */}
-          <div className="md:w-5/12 lg:w-6/12 text-center md:text-left lg:pl-8 space-y-4">
-            <p className="text-lg">
+          <div className="md:w-5/12 lg:w-[40%] flex flex-col gap-12 text-center md:text-left lg:pl-8 space-y-4 text-3xl [&_span]:text-secundary [&_span]:text-4xl ">
+            <p>
               <span className="font-semibold">Descripción:</span> {property.description}
             </p>
-            <p className="text-lg">
+            <p>
               <span className="font-semibold">Precio:</span> ${property.price}
             </p>
-            <p className="text-lg">
+            <p>
               <span className="font-semibold">Dirección:</span> {property.address}
             </p>
-            <p className="text-lg">
+            <p>
               <span className="font-semibold">Metros cuadrados:</span> {property.squareMeters} m²
             </p>
-            <p className="text-lg">
+            <p>
               <span className="font-semibold">Tipo de propiedad:</span> {property.propertyType}
             </p>
-            <p className="text-lg">
+            <p>
               <span className="font-semibold">Tipo de contrato:</span> {property.contractType}
             </p>
           </div>
@@ -65,9 +65,9 @@ const PropertyPage = () => {
 
         {/* Galería de Imágenes */}
         {property.images?.length > 0 && (
-          <section className="mt-10">
-            <h2 className="text-center text-xl font-semibold mb-4">Galería de Imágenes</h2>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+          <section className="mt-52">
+            <h2 className="text-center text-4xl font-semibold mb-52">Galería de Imágenes</h2>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 ">
               {property.images.map((image, index) => (
                 <GalleryItem key={index} imageSrc={image} imgAlt={`Imagen ${index + 1}`} />
               ))}
